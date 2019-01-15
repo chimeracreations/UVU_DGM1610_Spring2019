fcdaccaa03a5e08055ff7d437f6bafa2c7db6480
Kyle <10884088@my.uvu.edu>
2019-01-14 17:18:14 -0700

    DAy 3 class

---
+++ 2D_Game/Assets/Scripts/firstScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstScript : MonoBehaviour {
+
+    public int waterBottle = 60;
+    
+	// Use this for initialization
+	void Start () 
+    {
+       print(waterBottle); 
+        Debug.Log(waterBottle);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
