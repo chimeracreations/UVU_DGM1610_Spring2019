16ee55319a078ec0ed21bd6afc787ed1574c945f
kyle <10884088@my.uvu.edu>
2019-01-28 17:20:19 -0700

    Variable day

---
+++ 2D_Game/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+	// These are my variables
+	public int number;
+	public float speed;
+	public string name;
+
+	// Use this for initialization
+	void Start () 
+	{
+		/* 
+		This
+		is
+		a
+		multiline
+		comment
+		 */
+		// number = 10;
+		speed = 0.9435738f;
+		name = "Bob";
+		
+		print(name + " is " + number + " years old! " + speed);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position = new Vector3(number,0,0);
+	}
+}
