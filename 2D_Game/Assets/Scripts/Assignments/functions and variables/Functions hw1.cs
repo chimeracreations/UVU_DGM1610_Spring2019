a78ac527e55a9840a962ff06db13fc2ab936dcd3
Kyle Lorenzen <10884088@my.uvu.edu>
2019-02-04 19:40:19 -0700

    Commit for coding practice

---
+++ 2D_Game/Assets/Scripts/Assignments/functions and variables/Functions hw1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functionshw1 : MonoBehaviour {
+
+	// Public stream to designate the pet name
+	public string petName;
+
+	// Use this for initialization
+	void Start () 
+	{ 
+		Pet(petName);
+	}
+	
+	// Prints a comment based on the pet given
+	void Pet(string petState)
+	{
+
+		if(petState == "Cat")
+		{
+			print("Kitty cuddles are good cuddles!");
+		}
+		else if(petState == "Dog")
+		{
+			print("Man's best friend!");
+		}
+		else if(petState == "Bird")
+		{
+			print("A fine feather friend!");
+		}
+		else if(petState == "Burb")
+		{
+			print("Very stylish feathery friend!");
+		}
+		else if(petState == "Dragon")
+		{
+			print("Yeah, sure... right...");
+		}
+		else
+		{
+			print(petState + " is quite an exotic pet!");
+		}
+	}
+}
