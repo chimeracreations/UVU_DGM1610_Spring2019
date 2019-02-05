1c33c14e710c0eda418f516de4ff0e5c44c11987
Kyle Lorenzen <10884088@my.uvu.edu>
2019-02-04 21:04:22 -0700

    Cookies with the count variable practice

---
+++ 2D_Game/Assets/Scripts/Assignments/functions and variables/Functions3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Functions3 : MonoBehaviour {
+
+	public int cookiesNumber; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		print("Let's count cookies with the Count!");
+		print(cookiesNumber + " cookie(s) are falling from the sky AH AH AH!");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		cookiesNumber += 1;
+
+		print(cookiesNumber + " cookie(s) are falling from the sky AH AH AH!");
+	}
+}
