6c3d58f315bfe52043d3177168290fcace04d4be
Kyle <10884088@my.uvu.edu>
2019-02-11 17:22:01 -0700

    2/11/19 inheritance

---
+++ 2D_Game/Assets/Scripts/Assignments/Animals/Caracal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caracal : Feline {
+
+	public float roarVolume;
+
+	// Use this for initialization
+	void Start () 
+	{
+		print("Rawr!");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D_Game/Assets/Scripts/Assignments/CoinPickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowflakePickup : GenericPickUp 
+{
+	public GameObject playerCharacter;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "playerCharacter")
+		{
+			// collect snowflake
+		}
+		else
+		{
+			// dont collect snowflake
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
