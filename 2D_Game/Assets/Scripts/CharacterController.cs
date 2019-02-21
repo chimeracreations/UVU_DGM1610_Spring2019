4b0f380526f66038c026f6b59a455c6532fa9a51
Kyle <10884088@my.uvu.edu>
2019-02-20 17:28:57 -0700

    School day 2/20/19

--- 2D_Game/Assets/Scripts/CharacterController.cs
+++ 2D_Game/Assets/Scripts/CharacterController.cs
@@ -4,41 +4,51 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
 
+	// Player movement variables
 	public float moveSpeed;
 	public float jumpHeight;
 
+	// Player grounded variables
+	private bool isGrounded;
+	private bool hasJumped;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+		// starts bool variables for jumping
+		isGrounded = true;
+		hasJumped = false;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if(Input.GetKey(KeyCode.D))	
+		// move the character right when pressing D or Right Arrow
+		if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))	
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
 		}
-		else if(Input.GetKey(KeyCode.RightArrow))	
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else if(Input.GetKey(KeyCode.A))	
-		{
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
-		}
-		else if(Input.GetKey(KeyCode.LeftArrow))	
+		// move the character left if pressing the A or left arrow
+		else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))	
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed,GetComponent<Rigidbody2D>().velocity.y);
 		}
 		
-		if(Input.GetKey(KeyCode.W))
+		// character jump and double jump code.
+		if(Input.GetKeyDown(KeyCode.W) && ((isGrounded == true) || (hasJumped == true)))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+			if (hasJumped == true)
+			{
+				hasJumped = false;
+			}
+			else hasJumped = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 		}
-		else if(Input.GetKey(KeyCode.Space))
+		else if(Input.GetKeyDown(KeyCode.UpArrow))
 		{
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 		}
