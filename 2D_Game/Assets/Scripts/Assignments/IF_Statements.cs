d380202a0ddec64455352788f2cd938538769ec7
Kyle <10884088@my.uvu.edu>
2019-02-25 17:06:39 -0700

    If statement class day

---
+++ 2D_Game/Assets/Scripts/Assignments/IF_Statements.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IF_Statements : MonoBehaviour {
+
+	public string stopLight;
+	public bool isUtah = false;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(stopLight == "red")
+		{
+			if(!isUtah)
+			{
+				print("stop");
+			}
+			else
+			{
+				print("STOP YOU FOOL!");
+			}
+
+		}	
+		else if((stopLight == "yellow") && (!isUtah))
+		{
+			print("caution");
+		}
+		else if((stopLight == "yellow") && (isUtah))
+		{
+			print("SLOW DOWN OR YOU'LL KILL SOMEONE!");
+		}
+		else
+		{
+			print("go");
+		}
+	}
+}
