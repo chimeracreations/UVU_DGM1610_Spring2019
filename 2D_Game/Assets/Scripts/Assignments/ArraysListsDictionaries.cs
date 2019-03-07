83e78caac418f7e3851cfde42992884164b3340d
Kyle Lorenzen <10884088@my.uvu.edu>
2019-03-06 19:52:48 -0700

    Arrays and lists (I couldn't access the quiz but the secret word is ninja)

---
+++ 2D_Game/Assets/Scripts/Assignments/ArraysListsDictionaries.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysListsDictionaries : MonoBehaviour {
+
+	// Collection of CLient Variables
+	public string clientOne = "Greg";
+	public string clientTwo = "Kate";
+	public string clientThree = "Adam";
+	public string clientFour = "Mia";
+	public string clientFive = "Fred";
+
+	// Array Example
+	public string[] clientList = new string[]{"Greg", "Kate", "Adam", "Mia", "Fred"};
+
+, 	// List Example
+	public List<string> santasNaughtyList = new List<string>();
+
+	// ArrayList Example
+	public ArrayList inventory = new ArrayList();
+
+	// Use this for initialization
+	void Start () {
+		santasNaughtyList.Add("Jimmy");
+		santasNaughtyList.Add("Jenny");
+		santasNaughtyList.Add("Sam");
+		santasNaughtyList.Add("Ty");
+		santasNaughtyList.Add("Susie");
+
+		santasNaughtyList.Remove("Jimmy");
+
+		print(clientList[2]);
+		print(santasNaughtyList[3]);
+
+		inventory.Add(10);
+		inventory.Add("Bob");
+		inventory.Add(true);
+		inventory.Add(5.2234);
+
+		print(inventory[2]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
