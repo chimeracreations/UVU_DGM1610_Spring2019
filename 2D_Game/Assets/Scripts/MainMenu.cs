526ce650f4cceb68c4486161a05f97c506ecd45d
Kyle <10884088@my.uvu.edu>
2019-03-13 17:16:52 -0600

    Class day 3/13/19 main menu

---
+++ 2D_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour 
+{
+	// Indexed level to load  
+	public int levelToLoad;
+
+	// this function loads the scene based on the levelToLoad Variable after button pressed
+	public void LoadLevel()
+	{
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	// Quits the program after button pressed	
+	public void LevelExit()
+	{
+		Application.Quit();
+	}
+}
