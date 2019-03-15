729f8635963d2e17f5fe69345762fccbc5168ad6
Kyle Lorenzen <10884088@my.uvu.edu>
2019-03-14 22:38:05 -0600

    New character controller that allows for increased speed gains down slopes

---
+++ 2D_Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+public Transform player;
+public Vector3 cameraOffset;
+public SnowBurrController burr;
+public float characterOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// follows the player plus an offset
+		// need to add code to make more smooth
+		if(burr.faceRight == true)
+		{
+			transform.position = new Vector3 (player.position.x + cameraOffset.x - characterOffset, player.position.y + cameraOffset.y, player.position.z + cameraOffset.z);
+		}
+		if(burr.faceRight == false)
+		{
+			transform.position = new Vector3 (player.position.x + cameraOffset.x + characterOffset, player.position.y + cameraOffset.y, player.position.z + cameraOffset.z);
+		}
+
+	}
+}
--- 2D_Game/Assets/Scripts/SnowBurrController.cs
+++ 2D_Game/Assets/Scripts/SnowBurrController.cs
@@ -2,45 +2,138 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowBurrController : PhysicsObjects {
+public class SnowBurrController : MonoBehaviour {
 
-    public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
+    public float force;             //Floating point variable to store the player's movement speed.
+    public float maxSpeed;
+    public float acceleration;
+    public float jumpHeight;
+	public bool faceRight = true;
+    public float characterOffset;
+    Animator animationSpeed;
+    private bool isGrounded = false;
+	private bool hasJumped = false;
+	public Transform groundCheck;
+	public Transform groundCheck2;
+	public Transform groundCheck3;
+	public float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
 
-    private SpriteRenderer spriteRenderer;
-    private Animator animator;
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
     // Use this for initialization
-    void Awake () 
+    void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer> (); 
-        animator = GetComponent<Animator> ();
+        // Speed variable for animations
+		animationSpeed = GetComponent<Animator>();
+
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D> ();
     }
 
-    protected override void ComputeVelocity()
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
     {
-        Vector2 move = Vector2.zero;
 
-        move.x = Input.GetAxis ("Horizontal");
+        // check to see if on the ground
+		if((Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround)) || (Physics2D.OverlapCircle(groundCheck2.position, groundRadius, whatIsGround)) 
+			|| (Physics2D.OverlapCircle(groundCheck3.position, groundRadius, whatIsGround)))
+				isGrounded = true;
+		else isGrounded = false;
 
-        if (Input.GetButtonDown ("Jump") && grounded) {
-            velocity.y = jumpTakeOffSpeed;
-        } else if (Input.GetButtonUp ("Jump")) 
+		animationSpeed.SetBool ("Ground", isGrounded);
+
+		if(isGrounded)
+			hasJumped = false;
+
+		// falling animation cues
+		animationSpeed.SetFloat ("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
+
+		// character jump and double jump code.
+		if((isGrounded || !hasJumped) && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)))
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+			animationSpeed.SetBool("Ground", false);
+
+			if(!hasJumped && !isGrounded)
+				hasJumped = true;
+			
+		}
+		
+		// if pressing both, do nothing
+		if ((Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)))
+		{
+			return;
+		}
+
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveRight = 0;
+        float moveLeft = 0;
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            if (velocity.y > 0) {
-                velocity.y = velocity.y * 0.5f;
-            }
+            moveRight += acceleration;
+        }
+        
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveLeft += acceleration;    
         }
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        if (flipSprite) 
+        float moveVertical = 0;
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movementRight = new Vector2 (moveRight, moveVertical);
+        Vector2 movementLeft = new Vector2 (moveLeft, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        if (GetComponent<Rigidbody2D>().velocity.x <= maxSpeed)
         {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
+            rb2d.AddForce (movementRight * force);
         }
 
-        animator.SetBool ("grounded", grounded);
-        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
-
-        targetVelocity = move * maxSpeed;
+        if (GetComponent<Rigidbody2D>().velocity.x >= -maxSpeed)
+        {
+            rb2d.AddForce (-movementLeft * force);
+        }
     }
+
+    void Update ()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+        {
+			if(faceRight == true);
+			else
+			{
+				FlipCharacter ();
+			}
+        }
+        
+        if (GetComponent<Rigidbody2D>().velocity.x < 0)
+        {
+			if(faceRight == false);
+			else
+			{
+				FlipCharacter ();
+			}
+        }
+
+    }
+
+    void FlipCharacter ()
+		{
+			faceRight = !faceRight;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+			if(faceRight)
+			{
+				transform.position = new Vector2 ((GetComponent<Rigidbody2D>().position.x + characterOffset),GetComponent<Rigidbody2D>().position.y);
+			}
+			else if(!faceRight)
+			{
+				transform.position = new Vector2 ((GetComponent<Rigidbody2D>().position.x - characterOffset),GetComponent<Rigidbody2D>().position.y);
+			}
+		}
 }
