f33e76930d836139d68fe5d81ba7a75aab7004d1
Kyle <10884088@my.uvu.edu>
2019-03-25 17:16:31 -0600

    Class 3/25/19 ground enemy movement

---
+++ 2D_Game/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	public int bottles = 100;
+
+	public bool isRaining = true;
+
+	// Use this for initialization
+	void Start () {
+		// For loop
+		for(int i=1; i <= bottles; i++)
+		{
+			print(i + " bottles of whatever you want on the wall");
+		}
+
+		while(bottles > 10)
+		{
+			print("it is raining " + bottles + " bruh");
+			bottles--;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D_Game/Assets/Scripts/GroundEnemyControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemyControl : MonoBehaviour {
+
+	//Movement Variables
+	public float moveSpeed;
+	public bool moveRight;
+
+	// Wall Check
+	public Transform wallCheck;
+	public float wallCheckRadius;
+	public LayerMask whatIsWall;
+	public bool hittingWall;
+
+	//Edge Check
+	public bool notAtEdge;
+	public Transform edgeCheck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+		hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+		if(hittingWall || !notAtEdge)
+		{
+			moveRight = !moveRight;
+		}
+
+		if (moveRight)
+		{
+			// the transform flips the sprite but will enlarge or shrink to the values in the Vector3
+			transform.localScale = new Vector3(-0.2f, 0.2f, 1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else
+		{
+			transform.localScale = new Vector3(0.2f, 0.2f, 1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		
+	}
+}
