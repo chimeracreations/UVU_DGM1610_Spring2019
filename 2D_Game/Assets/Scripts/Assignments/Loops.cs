d6ba77089b53d62a0024cba90c196ed839916ac4
Kyle <10884088@my.uvu.edu>
2019-03-27 17:07:39 -0600

    class day 3/27/19

--- 2D_Game/Assets/Scripts/Assignments/Loops.cs
+++ 2D_Game/Assets/Scripts/Assignments/Loops.cs
@@ -9,17 +9,39 @@ public class Loops : MonoBehaviour {
 	public bool isRaining = true;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		// For loop
 		for(int i=1; i <= bottles; i++)
 		{
 			print(i + " bottles of whatever you want on the wall");
 		}
 
+		// while loop
 		while(bottles > 10)
 		{
 			print("it is raining " + bottles + " bruh");
 			bottles--;
+		}
+
+		// do while loop
+		bool shouldContinue = false;
+		do
+		{
+			print("Hello World");
+		}
+		while(shouldContinue == true);
+
+		// For Each Loop
+		string[] strings = new string[3];
+
+		strings[0] = "first string";
+		strings[1] = "second string";
+		strings[2] = "third string";
+		
+		foreach(string item in strings)
+		{
+			print (item);
 		}
 	}
 	
---
+++ 2D_Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckPoint;
+	private Rigidbody2D pcRigid;
+	
+	private GameObject player;
+
+	// Particles are fun
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
+
+	//Respawn delay
+	public float respawnDelay;
+
+
+	//Point penalty on death
+	public int pointPenaltyOnDeath;
+	
+	// Store gravity value
+	private float gravityStore;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		pcRigid = GameObject.Find("SnowBurr").GetComponent<Rigidbody2D>();
+		player = GameObject.Find("SnowBurr");
+	}
+		
+
+}
