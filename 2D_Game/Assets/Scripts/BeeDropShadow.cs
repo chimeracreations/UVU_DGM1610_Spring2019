3d37f36c40f9b28c0bdd5ba86f3e5b73fe845cc1
Kyle Lorenzen <10884088@my.uvu.edu>
2019-03-31 16:33:24 -0600

    Worked on a kill enemy script

---
+++ 2D_Game/Assets/Scripts/BeeDropShadow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeDropShadow : MonoBehaviour {
+
+	
+	public float offset;
+	public Transform bee;
+	float distance;
+	public int layerMask = 9;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		RaycastHit2D hit = Physics2D.Raycast(bee.position, -Vector2.up, Mathf.Infinity, layerMask);
+        if(hit.collider != null)
+		{
+			distance = Mathf.Abs(hit.point.y - bee.position.y);
+		}
+
+		transform.position = new Vector3 (bee.position.x, bee.position.y - offset - distance, bee.position.z);
+		
+		var slopeRotation = Quaternion.FromToRotation (transform.up, hit.normal);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, slopeRotation * transform.rotation, 10 * Time.deltaTime);
+	}
+}
---
+++ 2D_Game/Assets/Scripts/KillEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillEnemy : MonoBehaviour {
+
+	public GameObject enemy;
+	public GameObject shadow;
+	public float delay = 0f;
+	public GameObject burr;
+ 
+    // Use this for initialization
+    void Start () 
+	{
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.gameObject.tag == ("Player") && burr.GetComponent<Rigidbody2D>().velocity.y < 0)
+		{
+			Destroy (enemy);//, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
+			Destroy (shadow);//, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
+		}
+	}
+}
