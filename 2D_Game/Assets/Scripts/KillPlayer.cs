f189e2cb00cd2a3eb13838a7a6eac04ff802f1de
Kyle <10884088@my.uvu.edu>
2019-04-01 17:16:01 -0600

    class 4/1/19

---
+++ 2D_Game/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		levelManager = FindObjectOfType <LevelManager>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.name == "SnowBurr")
+		{
+			levelManager.RespawnPlayer();
+		}
+	}
+
+}
--- 2D_Game/Assets/Scripts/LevelManager.cs
+++ 2D_Game/Assets/Scripts/LevelManager.cs
@@ -22,6 +22,7 @@ public class LevelManager : MonoBehaviour {
 	
 	// Store gravity value
 	private float gravityStore;
+	private float gravityScale;
 
 
 	// Use this for initialization
@@ -30,6 +31,36 @@ public class LevelManager : MonoBehaviour {
 		pcRigid = GameObject.Find("SnowBurr").GetComponent<Rigidbody2D>();
 		player = GameObject.Find("SnowBurr");
 	}
-		
+	
+	public void RespawnPlayer()
+	{
+		StartCoroutine("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo()
+	{
+		// Generate Death Particle
+		Instantiate (deathParticle, pcRigid.transform.position, pcRigid.transform.rotation);
+		// Hide pcRigid
+		player.SetActive(false);
+		player.GetComponent<Renderer>().enabled = false;
+		// Gravity Reset
+		gravityStore = pcRigid.GetComponent<Rigidbody2D>().gravityScale;
+		pcRigid.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		pcRigid.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		// Point Penalty
+		ScoreManager.AddPoints(-pointPenaltyOnDeath);
+		// Debug Message
+		yield return new WaitForSeconds (respawnDelay);
+		// Gravity Restore
+		pcRigid.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+		// Match players Transform position
+		pcRigid.transform.position = currentCheckPoint.transform.position;
+		// Show Player
+		player.SetActive(true);
+		player.GetComponent<Renderer>().enabled = true;
+		// Spawn Player
+		Instantiate (respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+	}
 
 }
