fdb70adccafcf02cd5808e0bb34fbebbdac7480f
Kyle Lorenzen <10884088@my.uvu.edu>
2019-04-18 14:06:52 -0600

    IT WORKING!

---
+++ 2D_Game/Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour {
+
+	public GameObject nextLevel;
+	public GameObject lastLevel;
+	public SnowBurrController burr;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (burr.faceRight == true)
+		{
+			nextLevel.SetActive(true);
+			lastLevel.SetActive(false);
+		}
+		if (burr.faceRight == false)
+		{
+			nextLevel.SetActive(false);
+			lastLevel.SetActive(true);
+		}
+	}
+}
