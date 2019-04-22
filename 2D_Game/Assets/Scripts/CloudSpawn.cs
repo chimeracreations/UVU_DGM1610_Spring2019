49c3c495e3ec7e176bfb480dcfcc640df21a3b44
Kyle Lorenzen <10884088@my.uvu.edu>
2019-04-21 22:27:25 -0600

    animations and ui updates

---
+++ 2D_Game/Assets/Scripts/CloudSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawn : MonoBehaviour {
+
+	private float timer;
+	public GameObject cloud;
+	public Transform cloudLocation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer -= Time.deltaTime;
+     	if (timer <= 0f)
+     	{
+        	Instantiate(cloud, transform.position, transform.rotation);
+        	timer = 18f;
+     	}	
+	}
+}
--- 2D_Game/Assets/Scripts/KillEnemy.cs
+++ 2D_Game/Assets/Scripts/KillEnemy.cs
@@ -12,11 +12,13 @@ public class KillEnemy : MonoBehaviour {
 	public SnowBurrController burrController;
 	public int pointsForKill;
 	public LevelManager manage;
+	Animator anim;
  
     // Use this for initialization
     void Start () 
 	{
-
+		anim = GetComponent<Animator>();
+		anim.SetBool ("dead", false);
     }
 	
 	// Update is called once per frame
@@ -28,13 +30,24 @@ public class KillEnemy : MonoBehaviour {
 	{
 		if (col.gameObject.tag == ("Player") && burr.GetComponent<Rigidbody2D>().velocity.y < velocityCheck)
 		{
-			Destroy (enemy);//, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
-			Destroy (shadow);//, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
-			Destroy(transform.parent.gameObject);
+			if (enemy.name == "IceBee")
+				Destroy (enemy.GetComponent<IceBeeMovement>());
+			anim.SetBool ("dead", true);
+			StartCoroutine(DeathAfterTime(0.5f));
 			burr.GetComponent<Rigidbody2D>().velocity = new Vector2(burr.GetComponent<Rigidbody2D>().velocity.x, burrController.jumpHeight);
 			if(burrController.isGrounded == false)
 				manage.enemyMultiplier++;
 			ScoreManager.AddPoints(pointsForKill * manage.enemyMultiplier);
 		}
 	}
+
+	IEnumerator DeathAfterTime(float time)
+ {
+    yield return new WaitForSeconds(time);
+	Destroy (enemy);
+	Destroy (shadow);//, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay); 
+	Destroy(transform.parent.gameObject);
+ }
 }
+
+
